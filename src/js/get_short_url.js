d9a3c65c44ac7b110663f44c138f43780d207333
Tomi Novak <tominovak33@gmail.com>
2015-06-15 23:26:42 +0100

    clean up code - separate into functions

--- src/js/get_short_url.js
+++ src/js/get_short_url.js
@@ -8,14 +8,11 @@ function get_short_url(input_url) {
     var full_url_value = document.getElementById('url').value;
     //call the api function with my callback
     var short_url_api_return = send_api_request(api_domain + 'api', 'url', full_url_value, function(result) {
-        result = JSON.parse(result);
-        var short_url = result.short_url;
-        var response_container = document.getElementById('response-wrapper');
+        var short_url = short_url_from_api_return(result);
         var response_data = document.getElementById('response-data');
-        var ui_container = document.getElementById('ui-wrapper');
-        response_data.innerHTML = api_domain+short_url;
-        ui_container.className = 'hidden';
-        response_container.className = '';
+        var response_link = create_short_url_link(api_domain, short_url);
+        response_data.appendChild(response_link);
+        display_response();
     });
 }
 
@@ -37,4 +34,16 @@ function send_api_request(url, parameter, value, callback) {
     var full_request_url = url+'/?'+parameter+'='+value;
     xmlhttp.open("GET",full_request_url,true);
     xmlhttp.send();
+}
+
+function create_short_url_link (domain, short_url) {
+    var response_link = document.createElement('a');
+    response_link.href = domain+short_url;
+    response_link.innerHTML = short_url;
+    return response_link;
+}
+
+function short_url_from_api_return(returned_date ){
+    returned_date = JSON.parse(returned_date);
+    return returned_date.short_url;
 }
--- src/js/user_interface_actions.js
+++ src/js/user_interface_actions.js
@@ -7,3 +7,17 @@ var get_short_url_action = document.getElementById("shorten-action");
 get_short_url_action.addEventListener("click", function(){
     get_short_url();
 });
+
+function display_ui () {
+    var response_container = document.getElementById('response-wrapper');
+    var ui_container = document.getElementById('ui-wrapper');
+    ui_container.className = '';
+    response_container.className = 'hidden';
+}
+
+function display_response () {
+    var response_container = document.getElementById('response-wrapper');
+    var ui_container = document.getElementById('ui-wrapper');
+    ui_container.className = 'hidden';
+    response_container.className = '';
+}
