33cb131f9f6af9de97204c77d43c0445e3166bef
Tomi <dev.tomi33@gmail.com>
2015-10-11 02:39:02 +0100

    add initial search box

---
+++ search/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tomi on 11/10/15.
+ */
+
+ var tag_input = document.getElementById("url_input");
+ tag_input.onkeyup = function(){
+    //document.getElementById('foo').innerHTML = inputBox.value;
+    console.log(tag_input.value);
+ };
