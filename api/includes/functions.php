d4fc849e44147d125e379db5b95c09f491827838
Tomi Novak <dev.tomi33@gmail.com>
2015-10-28 12:58:45 +0000

    increase search limit

--- api/includes/functions.php
+++ api/includes/functions.php
@@ -169,7 +169,7 @@ function search_url_database($search_term) {
     $fields = "*";
     $criteria = "`url_short` LIKE '%$search_term%'";
     $sql_query = select_query_builder('url_table' , $fields, $criteria);
-    $search_lenght = strlen($search_term);
+    $search_lenght = strlen($search_term)+2;
     $limit = ($search_lenght*$search_lenght);
     $sql_query .= "LIMIT "."$limit";
     $query_result = db_query($sql_query);
